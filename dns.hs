36e148cf6c297b3c98d3548fa15e3c943d1aca92
Benjamin Smith <polynomial@gmail.com>
2017-10-18 22:12:37 -0700

    working main

--- dns.hs
+++ dns.hs
@@ -1,8 +1,9 @@
 import Network.DNS
 import Network.DNS.Lookup
+import Data.ByteString.Char8
+main = do
 let hostname = Data.ByteString.Char8.pack "www.huugs.com"
-in
-  rs <- makeResolvSeed defaultResolvConf
-  withResolver rs $ \resolver -> lookupA resolver hostname
+rs <- makeResolvSeed defaultResolvConf
+withResolver rs $ \resolver -> lookupA resolver hostname
 
 
