4985cd33a81d07ea7b6b487b627fb4bc9080eddf
Benjamin Smith <polynomial@gmail.com>
2017-10-18 22:30:58 -0700

    Notes from repl, change variable name

--- dns.hs
+++ dns.hs
@@ -1,9 +1,35 @@
 import Network.DNS
 import Network.DNS.Lookup
 import Data.ByteString.Char8
+-- something about overloaded strings to read up on later:
+-- http://www.haskell.org/ghc/docs/7.6.3/html/users_guide/type-class-extensions.html#overloaded-strings
+
 main = do
+
 let hostname = Data.ByteString.Char8.pack "www.huugs.com"
-rs <- makeResolvSeed defaultResolvConf
-withResolver rs $ \resolver -> lookupA resolver hostname
+resolvSeed <- makeResolvSeed defaultResolvConf
+-- makeResolvSeed :: ResolvConf -> IO ResolvSeed
+-- defaultResolvConf :: ResolvConf
+-- rs :: ResolvSeed
+
+
+
+withResolver resolvSeed $ \resolver -> lookupA resolver hostname
+-- withResolver :: ResolvSeed -> (Resolver -> IO a) -> IO a
+
+
+-- things i learned:
+-- can i replace rs:
+-- Prelude Network.DNS.Lookup Network.DNS> withResolver (makeResolvSeed defaultResolvConf) $ \resolver -> lookupA resolver hostname
+-- <interactive>:12:15: error:
+--     • Couldn't match expected type ‘ResolvSeed’
+--                   with actual type ‘IO ResolvSeed’
+--     • In the first argument of ‘withResolver’, namely
+--         ‘(makeResolvSeed defaultResolvConf)’
+--       In the expression: withResolver (makeResolvSeed defaultResolvConf)
+--       In the expression:
+--         withResolver (makeResolvSeed defaultResolvConf)
+--         $ \ resolver -> lookupA resolver hostname
+-- the <- 'binding' removes the IO (performs it?)
 
 
